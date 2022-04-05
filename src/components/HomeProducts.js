45a01f0c332f4ca41d718af3b53d6e7ac96ea53b
Dan Ploesteanu <ploesteanudan.99@gmail.com>
2022-04-05 14:27:22 +0300

    homeProducts 2

--- src/components/HomeProducts.js
+++ src/components/HomeProducts.js
@@ -1,13 +1,20 @@
 import React from "react";
 import products from "../assets/json/products.json";
 import Product from "../components/Product";
+import { CaretRight } from "phosphor-react";
 import "../styles/homeProducts.scss";
 export default function HomeProducts() {
   return (
     <div className="homeProducts">
-      {products.products.map((product, index) => (
-        <Product product={product} />
-      ))}
+      <div className="homeProductsProducts">
+        {products.products.map((product, index) => (
+          <Product product={product} />
+        ))}
+      </div>
+      <div className="homeProductsLink">
+        <p>Shop now</p>
+        <CaretRight size={17} />
+      </div>
     </div>
   );
 }
