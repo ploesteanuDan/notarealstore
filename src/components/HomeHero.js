c4d265261d42262163cbe9cbd5a33577bc90b698
Dan Ploesteanu <ploesteanudan.99@gmail.com>
2022-04-05 20:49:35 +0300

    home hero tablet

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -10,8 +10,7 @@ export default function HomeHero() {
         <p className="description">
           Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit.
+          ab illo inventore veritatis
         </p>
         <div className="button">
           <p>Shop now</p>
