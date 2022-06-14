28dc8a9943795adc5eb2a8147bc7b30e8281a297
Dan Ploesteanu <ploesteanudan.99@gmail.com>
2022-06-14 16:54:34 +0300

    product testing works

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,29 +4,25 @@ import { Link } from "react-router-dom";
 import "../styles/product.scss";
 export default function Product(props) {
   return (
-    <div data-testid="prod-1">
-      {props.product && (
-        <Link to={"/product/" + props.product.product_variation_id}>
-          <div className="product">
-            <div className="picContainer">
-              <img
-                src={props.product.product_picture_url}
-                alt={props.product_name}
-              />
-            </div>
-            <span>
-              <div className="left">
-                <p className="producer">{props.product.producer_name}</p>
-                <p className="name">{props.product.product_name}</p>
-              </div>
-              <div className="right">
-                <p>$</p>
-                <p>{props.product.price}</p>
-              </div>
-            </span>
+    <Link to={"/product/" + props.product.product_variation_id}>
+      <div className="product" data-testid="p1">
+        <div className="picContainer">
+          <img
+            src={props.product.product_picture_url}
+            alt={props.product_name}
+          />
+        </div>
+        <span>
+          <div className="left">
+            <p className="producer">{props.product.producer_name}</p>
+            <p className="name">{props.product.product_name}</p>
           </div>
-        </Link>
-      )}
-    </div>
+          <div className="right">
+            <p>$</p>
+            <p>{props.product.price}</p>
+          </div>
+        </span>
+      </div>
+    </Link>
   );
 }
--- src/tests/Product.test.js
+++ src/tests/Product.test.js
@@ -1,8 +1,22 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import Product from "../components/Product";
-
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+const dummy = {
+  product_variation_id: 1,
+  product_picture_url:
+    "https://images.unsplash.com/photo-1655143905566-b824848908c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
+  product_name: "Ultraboost",
+  producer_name: "Adidas",
+  price: 120,
+};
 test("test", () => {
-  render(<Product />);
-  const productElement = screen.getByTestId("prod-1");
-  //expect(productElement).toBeInTheDocument();
+  render(
+    <BrowserRouter>
+      <Product product={dummy} />
+    </BrowserRouter>
+  );
+  const productElement = screen.getByTestId("p1");
+  expect(productElement).toBeInTheDocument();
+  expect(productElement).toHaveTextContent("Adidas");
 });
